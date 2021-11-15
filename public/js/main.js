6efaa4a5939ca21f45d3bdd7f38c41a3aabfd5da
mariosrzz <mariosm1994@gmail.com>
2021-11-15 11:30:47 +0100

    updated css card

--- public/js/main.js
+++ public/js/main.js
@@ -4,3 +4,17 @@ $(".delete-card-item").click(function() {
     $("#delete-card-button").prop("href", `/delete_card/${cardId}`)
 })
 
+
+$("#create-card-button").click(function() {
+    console.log("Click")
+    const name = $('input[name="name"]').val()
+    const description = $('input[name="description"]').val()
+    const price = $('input[name="price"]').val()
+})
+
+
+$('input[name="name"]').change(function() {
+    console.log("Cambio")
+    console.log($(this).val())
+    $("#create-card-button").prop("disabled",false)
+})
